bb9d7b2247aa19c2949feb2bf34870579b1b5191
John Dewey <john@dewey.ws>
2012-09-22 21:51:01 -0700

    Corrected versioning

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "john@dewey.ws"
 license          "Apache 2.0"
 description      "Installs/Configures reboot-handler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "0.1.0"
 
 recipe           "reboot-handler", "Installs/Configures reboot-handler"
 
