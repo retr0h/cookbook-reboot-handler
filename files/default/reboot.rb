41d1bdf5312cd29ab68e620cd2574dbd0b5a747b
John Dewey <john@dewey.ws>
2012-10-27 12:39:47 -0700

    Corrected nesting

--- files/default/reboot.rb
+++ files/default/reboot.rb
@@ -6,12 +6,12 @@ class Reboot < Chef::Handler
     ### If Chef ran successfully.
     if run_status.success?
       ### AND node is in the booted role.
-        if node.roles.include? node['reboot-handler']['enabled_role']
-          ### AND node has the reboot flag.
-          if node.run_state['reboot']
-            ### THEN reboot node.
-            ::Chef::ShellOut.new(node['reboot-handler']['reboot_command']).run_command
-          end
+      if node.roles.include? node['reboot-handler']['enabled_role']
+        ### AND node has the reboot flag.
+        if node.run_state['reboot']
+          ### THEN reboot node.
+          ::Chef::ShellOut.new(node['reboot-handler']['reboot_command']).run_command
+        end
       end
     end
   end
