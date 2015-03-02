38036cd8fb39e9d994024b4c0efcb238c8595b08
Chris Manson <chris@manson.ie>
2015-03-02 17:57:01 +0000

    Adding source_url and issues_url
    Just adding these so the supermarket shows up the link to the cookbook code.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description 'Installs/Configures reboot-handler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
 
+source_url  'https://github.com/retr0h/cookbook-reboot-handler/'
+issues_url  'https://github.com/retr0h/cookbook-reboot-handler/issues'
+
 recipe 'reboot-handler', 'Installs/Configures reboot-handler'
 
 %w(debian ubuntu).each do |os|
